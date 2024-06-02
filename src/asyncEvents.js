8ffe21f8710894724e16c53b5048f9bfbb46857e
James Jeffers <jdjeffers@protonmail.com>
2024-06-02 15:05:33 -0400

    Fixes issue where report generation submission wasn't completing due to lack of await.

---
+++ src/asyncEvents.js
@@ -0,0 +1,129 @@
+import Resolver from '@forge/resolver';
+import api, { route } from "@forge/api";
+import { storage, startsWith } from "@forge/api";
+import { Queue, JobDoesNotExistError } from '@forge/events';
+
+import { calculateConfidenceIntervals, getCountsPerPeriod } from "./resolvers/calculations";
+
+const asyncResolver = new Resolver();
+const queueName = 'reports';
+const queue = new Queue({ key: queueName});
+
+const getTrailing15WeeksClosedIssues = async (projectId) => {
+  let results = [];
+  let next = true;
+
+  console.log("getting trailing 15 weeks closed issues...")
+  const request_route = route`/rest/api/3/search?jql=project=${projectId} AND status=Done AND resolved >= -15w&limit=100&startAt=0`;
+
+  while (next) {
+    const response = await api.asApp()
+      .requestJira(request_route, {
+        headers: {
+          'Accept': 'application/json'
+        }
+    });
+
+    const data = await response.json();
+
+    results = results.concat(data.issues);
+    data.next? next = data.next : next = false;
+  }
+
+  return results;
+}
+
+const getCurrentBacklogIssues = async (projectId) => {
+    let results = [];
+    let next = true;
+  
+    console.log("getting current backlog issues...")
+    const request_route = route`/rest/api/3/search?jql=project=${projectId} AND Resolution is NULL ORDER BY Rank ASC&limit=100&startAt=0`;
+  
+    while (next) {
+      const response = await api.asApp()
+        .requestJira(request_route, {
+          headers: {
+            'Accept': 'application/json'
+          }
+      });
+  
+      const data = await response.json();
+      console.log(`Response: ${response.status} ${response.statusText}`);
+      results = results.concat(data.issues);
+      next = false;
+    }
+  
+    return results;
+}
+
+asyncResolver.define("event-listener", async ({ payload, context }) => {
+
+    const projectId = payload.projectId;
+  
+    console.log(`Checking storage for current report jobs...`);
+    const currentJobs = await storage.query().limit(19)
+      .where('key', startsWith('job:' + projectId))
+      .getMany();
+  
+    currentJobs.results.map((job) => {
+      const jobId = job.key.split(':')[2];
+      console.log(`Current job id ${jobId} found in storage.`);
+  
+      try {
+        const jobProgress = queue.getJob(jobId);
+        const response = jobProgress.getStats();
+        const {success, inProgress, failed} = response.json();
+        console.log(`Job progress: ${success} success, ${inProgress} in progress, ${failed} failed.`);
+      }
+      catch(error) {
+        if (error instanceof JobDoesNotExistError) {
+          console.log(`Job ${jobId} does not exist in the queue. Removing from storage...`);
+          storage.delete(currentJob.key);
+        }
+        else {
+          console.log(error)
+        }
+      }
+   
+      return
+    });
+    
+    const response = await api.asApp()
+      .requestJira(route`/rest/api/3/project/${projectId}`, {
+        headers: {
+          'Accept': 'application/json'
+        }
+    });
+  
+    const projectData = await response.json();
+  
+    console.log(`Project data: ${projectData.name} id ${projectData.id}`);
+  
+    const trailing15WeeksIssuesClosed = await getTrailing15WeeksClosedIssues(projectId);
+    console.log(`Trailing 15 weeks issues closed found ${trailing15WeeksIssuesClosed.length} issues.`);
+  
+    const countsByPeriod = getCountsPerPeriod(trailing15WeeksIssuesClosed, 1);
+    console.log(`Counts by period: ${JSON.stringify(countsByPeriod)}`);
+  
+    const currentBacklogIssues = await getCurrentBacklogIssues(projectId);
+    console.log(`Current backlog issues found ${currentBacklogIssues.length} issues.`);
+    console.log(`example issue: ${JSON.stringify(currentBacklogIssues[0])}`)
+    const reportIssues = calculateConfidenceIntervals(currentBacklogIssues, countsByPeriod);
+    console.log(`Confidence intervals: ${JSON.stringify(confidenceIntervals)}`);
+  
+    
+    const report = {
+      created_at: new Date(),
+      project_id: projectData.id,
+      project_name: projectData.name,
+      counts_by_period: {},
+      lines: reportIssues
+    }
+  
+    const currentReport = await storage.set(projectId, JSON.stringify(report));
+  });
+  
+  
+  export const asyncJobHandler = asyncResolver.getDefinitions();
+  
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,63 +1,13 @@
 import Resolver from '@forge/resolver';
 import api, { route } from "@forge/api";
 import { storage, startsWith } from "@forge/api";
-import { Queue, JobDoesNotExistError } from '@forge/events';
+import { Queue } from '@forge/events';
 
-import { calculateConfidenceIntervals, getCountsPerPeriod } from "./calculations";
 import { isStaleReport } from "./utils";
-import { invoke } from '@forge/bridge';
 
 const resolver = new Resolver();
 const queueName = 'reports';
-const queue = new Queue({ key: queueName});
-
-const getTrailing15WeeksClosedIssues = async (projectId) => {
-  let results = [];
-  let next = true;
-
-  console.log("getting trailing 15 weeks closed issues...")
-  const request_route = route`/rest/api/3/search?jql=project=${projectId} AND status=Done AND resolved >= -15w&limit=100&startAt=0`;
-
-  while (next) {
-    const response = await api.asApp()
-      .requestJira(request_route, {
-        headers: {
-          'Accept': 'application/json'
-        }
-    });
-
-    const data = await response.json();
-
-    results = results.concat(data.issues);
-    data.next? next = data.next : next = false;
-  }
-
-  return results;
-}
-
-const getCurrentBacklogIssues = async (projectId) => {
-    let results = [];
-    let next = true;
-  
-    console.log("getting current backlog issues...")
-    const request_route = route`/rest/api/3/search?jql=project=${projectId} AND Resolution is NULL ORDER BY Rank ASC&limit=100&startAt=0`;
-  
-    while (next) {
-      const response = await api.asApp()
-        .requestJira(request_route, {
-          headers: {
-            'Accept': 'application/json'
-          }
-      });
-  
-      const data = await response.json();
-      console.log(`Response: ${response.status} ${response.statusText}`);
-      results = results.concat(data.issues);
-      next = false;
-    }
-  
-    return results;
-}
+const queue = new Queue({ key: queueName });
 
 async function generateCurrentReport(projectId) {
   console.log(`Queing new report for ${projectId}...`);
@@ -81,11 +31,11 @@ resolver.define('getCurrentReport', async (req) => {
 
   if (Object.keys(currentReport).length === 0) {
     console.log(`No cached report found for project id ${projectId}, generating one now...`);
-    generateCurrentReport(projectId);
+    await generateCurrentReport(projectId);
   }
   else if (isStaleReport(currentReport.created_at)) {
     console.log(`Cached report found for project id ${projectId}, created at ${new Date(currentReport.created_at)} is stale, generating a new one...`);
-    generateCurrentReport(projectId);
+    await generateCurrentReport(projectId);
   }
 
   if (currentReport) {
@@ -113,74 +63,5 @@ resolver.define('getProjects', async (req) => {
 
     return projectData;
 })
-
-resolver.define("event-listener", async ({ payload, context }) => {
-
-  const projectId = payload.projectId;
-
-  console.log(`Checking storage for current report jobs...`);
-  const currentJobs = await storage.query().limit(19)
-    .where('key', startsWith('job:' + projectId))
-    .getMany();
-
-
-  currentJobs.results.map((job) => {
-    const jobId = job.key.split(':')[2];
-    console.log(`Current job id ${jobId} found in storage.`);
-
-    try {
-      const jobProgress = queue.getJob(jobId);
-      const response = jobProgress.getStats();
-      const {success, inProgress, failed} = response.json();
-      console.log(`Job progress: ${success} success, ${inProgress} in progress, ${failed} failed.`);
-    }
-    catch(error) {
-      if (error instanceof JobDoesNotExistError) {
-        console.log(`Job ${jobId} does not exist in the queue. Removing from storage...`);
-        storage.delete(currentJob.key);
-      }
-      else {
-        console.log(error)
-      }
-    }
- 
-    return
-  });
-  
-  const response = await api.asApp()
-    .requestJira(route`/rest/api/3/project/${projectId}`, {
-      headers: {
-        'Accept': 'application/json'
-      }
-  });
-
-  const projectData = await response.json();
-
-  console.log(`Project data: ${projectData.name} id ${projectData.id}`);
-
-  const trailing15WeeksIssuesClosed = await getTrailing15WeeksClosedIssues(projectId);
-  console.log(`Trailing 15 weeks issues closed found ${trailing15WeeksIssuesClosed.length} issues.`);
-
-  const countsByPeriod = getCountsPerPeriod(trailing15WeeksIssuesClosed, 1);
-  console.log(`Counts by period: ${JSON.stringify(countsByPeriod)}`);
-
-  const currentBacklogIssues = await getCurrentBacklogIssues(projectId);
-  console.log(`Current backlog issues found ${currentBacklogIssues.length} issues.`);
-  console.log(`example issue: ${JSON.stringify(currentBacklogIssues[0])}`)
-  const reportIssues = calculateConfidenceIntervals(currentBacklogIssues, countsByPeriod);
-  console.log(`Confidence intervals: ${JSON.stringify(confidenceIntervals)}`);
-
-  
-  const report = {
-    created_at: new Date(),
-    project_id: projectData.id,
-    project_name: projectData.name,
-    counts_by_period: {},
-    lines: reportIssues
-  }
-
-  const currentReport = await storage.set(projectId, JSON.stringify(report));
-});
-
 
 export const handler = resolver.getDefinitions();
